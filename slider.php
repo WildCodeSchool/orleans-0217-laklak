1b682f5ae38b7f5f32a030c19765df8e720bebf5
Nicolas <lf.nicolas45@gmail.com>
2017-03-22 16:43:27 +0100

    ajout du header et de la page d'accueil

---
+++ slider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder12
+ * Date: 22/03/17
+ * Time: 14:39
+ */
+?>
+<section id="slider" class="swiper_wrapper full-screen clearfix">
+
+    <div class="">
+
+        <div class="swiper-container swiper-parent">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" style="background-image: url('images/Laklak/Slider/1-1.jpg');">
+                    <div class="container clearfix">
+                        <div class="slider-caption slider-caption-center">
+                            <h2 data-caption-animate="fadeInUp">LAKLAK <br/>PRODUCTION</h2>
+                            <p data-caption-animate="fadeInUp" data-caption-delay="200">Flexibility to the core &amp; different in every aspect. Meet the new Canvas. Creative &amp; Efficiently Powerful.</p>
+                            <a data-caption-animate="fadeInUp" data-caption-delay="300" href="#" class="button button-border button-rounded">View our Works</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" style="background-image: url('images/Laklak/Slider/2-1.jpg');">
+                    <div class="container clearfix">
+                        <div class="slider-caption slider-caption-center">
+                            <h2 data-caption-animate="fadeInUp">LAKLAK PRODUCTION</h2>
+                            <p data-caption-animate="fadeInUp" data-caption-delay="200">Flexibility to the core &amp; different in every aspect. Meet the new Canvas. Creative &amp; Efficiently Powerful.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
+            <div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
+        </div>
+
+    </div>
+
+</section>
