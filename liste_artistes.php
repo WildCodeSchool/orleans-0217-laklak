b00028ec1723a8c11d9331e5b75e1ff7b33bd1d8
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-03-28 15:49:11 +0200

    creation page liste artistes

---
+++ liste_artistes.php
@@ -0,0 +1,84 @@
+<?php
+include 'header.php';
+?>
+
+<body>
+
+<?php
+include 'navbar.php';
+?>
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2016/meryem2.jpg" alt="meryem aboulouafa">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2015/imedalibi.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2015/paraone.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="thumbnail">
+                    <div class="img-box">
+                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+<!-- Footer -->
+
+<?php
+include ('footer.php');
+?>
+
