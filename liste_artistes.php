1f25b96fc12a37a39c2284cf48b0ef92055a776d
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-03-29 15:12:08 +0200

    page liste_artistes

--- liste_artistes.php
+++ liste_artistes.php
@@ -9,65 +9,148 @@ include 'navbar.php';
 ?>
 
 
-    <div class="container">
+    <div class="container clearfix list_artist">
 
-        <div class="row">
+        <div class="heading-block">
+            <h3>ARTISTES</h3>
 
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+        </div>
+
+
+
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/meryem.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2016/meryem.jpg" alt="meryem"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">MERYEM</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2016/meryem2.jpg" alt="meryem aboulouafa">
-                    </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="images/Laklak/logo.jpg" alt=""/>
                 </div>
             </div>
+        </div>
 
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2015/imedalibi.jpg" alt="">
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2015/paraone.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2015/paraone.jpg" alt="meryem"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">PARAONE</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2015/paraone.jpg" alt="">
-                    </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="" alt=""/>
                 </div>
             </div>
+        </div>
 
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/ayoub.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2016/ayoub.jpg" alt="ayoub"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">AYOUB</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
-                    </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="" alt=""/>
                 </div>
             </div>
+        </div>
 
-            <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="thumbnail">
-                    <div class="img-box">
-                        <img class="" src="images/photoartistes/2016/meryem.jpg" alt="">
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/meryem.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2016/meryem.jpg" alt="meryem"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">MERYEM</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
                     </div>
                 </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="images/Laklak/logo.jpg" alt=""/>
+                </div>
             </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/habibifunk.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2016/habibifunkjpg" alt="meryem2"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">HABIBIFUNK</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="" alt=""/>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/meryem.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2016/meryem.jpg" alt="meryem"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">MERYEM</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="images/Laklak/logo.jpg" alt=""/>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 thumbnail bottommargin">
+            <div class="feature media-box">
+                <div class="fbox-media">
+                    <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/meryem.jpg')">
+                        <img class="img_list_artist" src="images/photoartistes/2016/meryem.jpg" alt="meryem"/>
+                        <a href="artiste.php"></a>
+                    </div>
+                    <div class="i-overlay on-hover">
+                        <a href="#"><span class="nom_list_artiste">MERYEM</span>
+                            <i class ="icon-line-plus"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="fbox-desc">
+                    <img class="list_label_laklak" src="images/Laklak/logo.jpg" alt=""/>
+                </div>
+            </div>
+        </div>
+
 
 
 
