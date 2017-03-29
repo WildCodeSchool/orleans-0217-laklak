ef249068e13f21abd84e960c13ee3a2a3822e0f7
Frankois21 <francois.ponceau@gmail.com>
2017-03-29 11:51:47 +0200

    page artiste

---
+++ artiste.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:bg-color="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <title>Artiste</title>
+
+<?php
+include 'header.php';
+?>
+</head>
+<?php
+include 'navbar.php';
+?>
+
+<body>
+
+    <div class="container">
+                    <div class=row">
+                             <div class="col-sm-4">
+                                    <div class="thumbnail">
+                                        <img src="images/events/7.jpg" class="img-responsive" alt="photo de l'artiste" style="width:100%; height:100%">
+                                    </div>
+                                      <div class="web"> <a href ="#" target ="_blank"><h3>Site de l'artiste</h3></a>
+                                      </div>
+                                     <div class="videoWrapper hidden-xs"><iframe src="https://www.youtube.com/embed/nOCmdZNgbQA" frameborder="0" allowfullscreen></iframe>
+                                     </div>
+
+                                     <div class="col-xs-offset-2">
+                                        <div> <a href="https://twitter.com/search?q=aboulouafa&src=typd&lang=fr" target="_blank" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
+                                            <i class="icon-twitter"></i>
+                                        </a>
+                                        </div>
+                                        <div>
+                                             <a href="https://www.facebook.com/meryemaboulouafa/" target="_blank" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+                                                <i class="icon-facebook"></i>
+                                            </a>
+                                        </div>
+                                        <div>
+                                             <a href="https://soundcloud.com/meryemaboulouafa" target="_blank" class="social-icon si-dark si-colored si-soundcloud nobottommargin" style="margin-right: 10px;">
+                                                 <i class="icon-soundcloud"></i>
+                                             </a>
+                                        </div>
+                                       
+                                     </div>
+                             </div>
+
+                                <div class="col-sm-8">
+                                        <h1>Meryem Aboulouafa</h1>
+                                        <div class="bio thumbnail"><h3>Biographie</h3>
+                                        <p>Auteure, compositeur et interprète vivant à Casablanca où elle est née en 1988, Meryem Aboulouafa participe, depuis plusieurs années, à de nombreux projets musicaux au Maroc et à l’étranger et initie diverses collaborations artistiques. Elle puise son inspiration au gré de ces rencontres, s’aguerrit sur scène en réalisant des performances en public, et enrichit petit à petit son univers artistique, jusqu’à décider de se consacrer au développement de son propre projet. Parmi ces différentes rencontres, celle avec Francesco Santalucia, compositeur et pianiste Italien, l’amènera à Rome, au sein du studio OMO. C’est là, qu’ils démarrent ensemble, en 2015, la réalisation d’un premier projet en commun.</p>
+                                        </div>
+                                        <div class="bio thumbnail"><h3>Dossier de presse</h3>
+                                            <p>Merci de cliquer ici pour téléchager mon dossier de presse</p>
+                                        </div>
+
+                                        <div class="bio thumbnail"><h3>Discographie</h3>
+                                        <p>Illud autem non dubitatur quod cum esset aliquando virtutum
+                                            omnium domicilium Roma, ingenuos advenas pleriqu nobilium,
+                                            ut Homerici bacarum suavitate Lotophagi, humanitatis
+                                            multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum
+                                            omnium domicilium Roma, ingenuos advenas pleriqu nobilium,
+                                            ut Homerici bacarum suavitate Lotophagi, humanitatis
+                                            multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum
+                                            omnium domicilium Roma, ingenuos advenas pleriqu nobilium,
+                                            ut Homerici bacarum suavitate Lotophagi, humanitatis
+                                            multiformibus officiis retentabant.Illud autem non dubitatur quod cum esset aliquando virtutum
+                                            omnium domicilium Roma, ingenuos advenas pleriqu nobilium,
+                                            ut Homerici bacarum suavitate Lotophagi, humanitatis
+                                            multiformibus officiis retentabant.</p>
+                                        </div>
+                                        <div class="bio thumbnail"><h3>Dates à retenir</h3>
+                                        <p>Un projet à retrouver sur scène pour la première fois en tournée, à partir du printemps 2017</p>
+                                        </div>
+
+
+                                        <div class="bio thumbnail"><h3>Actualités</h3>
+                                        <p>Illud autem non dubitatur quod cum esset aliquando virtutum
+                                            omnium domicilium Roma, ingenuos advenas pleriqu nobilium,
+                                            ut Homerici bacarum suavitate Lotophagi, humanitatis
+                                            multiformibus officiis retentabant.</p>
+                                        </div>
+
+
+
+                                    <div class="bio thumbnail"><h3>Gallerie</h3>        <!--Gallerie image -->
+
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/F94T0300.JPG">
+                                                <img src="images/Laklak/Meryem/F94T0300.JPG" alt="Meryem Aboulouafa" width="300" height="200">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/JohnFPeters_BDMOROC_5828.jpg">
+                                                <img src="images/Laklak/Meryem/JohnFPeters_BDMOROC_5828.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/Meryem%20Aboulouafa.jpg">
+                                                <img src="images/Laklak/Meryem/Meryem%20Aboulouafa.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-12.jpg">
+                                                <img src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-12.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-13.jpg">
+                                                <img src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-13.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-18.jpg">
+                                                <img src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-18.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-19.jpg">
+                                                <img src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-19.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+                                    <div class="responsive">
+                                        <div class="gallery">
+                                            <a target="_blank" href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-20.jpg">
+                                                <img src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-20.jpg" alt="Meryem Aboulouafa" width="600" height="400">
+                                            </a>
+                                            <div class="desc">Meryem Aboulouafa</div>
+                                        </div>
+                                    </div>
+
+                                    <div class="clearfix"></div>
+                                </div>
+                             </div>        <!--fin gallerie image-->
+                    </div>
+    </div>
+
+
+
+    <?php
+    include  'footer.php';
+    ?>
+</body>
+</html>
