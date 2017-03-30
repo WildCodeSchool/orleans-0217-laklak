b1eb984980f9372621588a72bc9d5d35310e8eb4
Frankois21 <francois.ponceau@gmail.com>
2017-03-30 21:14:03 +0200

    détail artiste

--- artiste.php
+++ artiste.php
@@ -10,48 +10,49 @@ include 'navbar.php';
 <body>
     <div class="container">
                     <div class=row">
-                             <div class="col-sm-4">
+                             <div class="col-sm-4 bottommargin detail-artiste">
+
                                  <h1 class="visible-xs">Meryem Aboulouafa</h1>
-                                    <div class="bio thumbnail">
+                        <div class="feature media-box clearfix">
+                                   <div class="detail fbox-media">
+                                       <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/meryem.jpg')">
+                                           <img class="img_list_artist" src="images/photoartistes/2016/meryem.jpg" alt="meryem"/>
+                                           <a href="artiste.php"></a>
+                                       </div>
+                                   </div>
+                        </div>
+                                        <div class="fbox-desc">
+                                                <img class="detail_list_label_laklak" src="images/Laklak/logo.jpg" alt=""/>
+                                            </div>
+                                 <!--      <div class="fbox-social">
 
-                                        <div class="feature media-box">
-                                            <div class="fbox-media">
-                                                <div class="image_list_artist" style="background-image:url('images/photoartistes/2016/meryem.jpg')">
-                                                    <img class="img_list_artist" src="images/photoartistes/2016/meryem.jpg" alt="meryem"/>
-                                                    <a href="artiste.php"></a>
-                                                </div>
-                                            </div>
-                                            <div class="fbox-desc">
-                                                <img class="list_label_laklak" src="images/Laklak/logo.jpg" alt=""/>
-                                            </div>
-                                            <div class="fbox-social">
-                                               <div class="site"> <a href="#" class="button button-xlarge button-dark button-rounded button-button border tright"<a href="#" class="button button-xlarge button-dark button-rounded tright">Site de l'artiste</a>
+                                                                                      <div class="social">
+                                                                                         <div class="site "> <a href="#" class="button button-xlarge button-dark button-rounded button-button border tright"<a href="#" class="button button-xlarge button-dark button-rounded tright">Site de l'artiste</a>
+                                                                                         </div>
+
+                                                                                           <a href="https://twitter.com/search?q=aboulouafa&src=typd&lang=fr" target="_blank" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
+                                                                                               <i class="icon-twitter"></i>
+                                                                                           </a>
+
+                                                                                           <a href="https://www.facebook.com/meryemaboulouafa/" target="_blank" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+                                                                                               <i class="icon-facebook"></i>
+                                                                                           </a>
+                                                                                           <a href="https://soundcloud.com/meryemaboulouafa" target="_blank" class="social-icon si-dark si-colored si-soundcloud nobottommargin" style="margin-right: 10px;">
+                                                                                               <i class="icon-soundcloud"></i>
+                                                                                           </a>
+
+                                                                                       -->
+
+                                              <div class="bio thumbnail video hidden-xs"><iframe width="100%" height="315" src="http://www.youtube.com/embed/nOCmdZNgbQA" frameborder="0" allowfullscreen></iframe>
                                                </div>
-                                                <div class="social">
-                                                     <a href="https://twitter.com/search?q=aboulouafa&src=typd&lang=fr" target="_blank" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
-                                                            <i class="icon-twitter"></i>
-                                                        </a>
-
-                                                        <a href="https://www.facebook.com/meryemaboulouafa/" target="_blank" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
-                                                            <i class="icon-facebook"></i>
-                                                        </a>
-                                                        <a href="https://soundcloud.com/meryemaboulouafa" target="_blank" class="social-icon si-dark si-colored si-soundcloud nobottommargin" style="margin-right: 10px;">
-                                                            <i class="icon-soundcloud"></i>
-                                                        </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-
-
-                                         <div class="bio thumbnail video hidden-xs"><iframe width="100%" height="315" src="http://www.youtube.com/embed/nOCmdZNgbQA" frameborder="0" allowfullscreen></iframe>
-                                         </div>
 
                                          <div class="bio thumbnail sound">
                                          <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/9943443&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
                                          </div>
+                                 <button class="button button-small button-border button-rounded tright nomargin" type="submit" id="template-contactform-submit"
+                                         name="template-contactform-submit" value="submit">Filtrer
+                                 </button>
+
                              </div>
 
                                 <div class="col-sm-8">
@@ -59,9 +60,7 @@ include 'navbar.php';
                                         <div class="bio thumbnail"><h3>Biographie</h3>
                                         <p>Auteure, compositeur et interprète vivant à Casablanca où elle est née en 1988, Meryem Aboulouafa participe, depuis plusieurs années, à de nombreux projets musicaux au Maroc et à l’étranger et initie diverses collaborations artistiques. Elle puise son inspiration au gré de ces rencontres, s’aguerrit sur scène en réalisant des performances en public, et enrichit petit à petit son univers artistique, jusqu’à décider de se consacrer au développement de son propre projet. Parmi ces différentes rencontres, celle avec Francesco Santalucia, compositeur et pianiste Italien, l’amènera à Rome, au sein du studio OMO. C’est là, qu’ils démarrent ensemble, en 2015, la réalisation d’un premier projet en commun.</p>
                                         </div>
-                                        <div class="bio thumbnail"><h3>Dossier de presse</h3>
-                                            <p>Merci de cliquer ici pour téléchager mon dossier de presse</p>
-                                        </div>
+
 
                                         <div class="bio thumbnail"><h3>Discographie</h3>
                                         <p>Illud autem non dubitatur quod cum esset aliquando virtutum
@@ -90,26 +89,24 @@ include 'navbar.php';
                                             multiformibus officiis retentabant.</p>
                                         </div>
 
-                          <div class="col_full clearfix">
-                            <h3>Galerie</h3>
+                                          <div class="col_full clearfix">
+                                            <h3>Galerie</h3>
 
-                            <div class="masonry-thumbs col-4" data-big="5" data-lightbox="gallery">
-                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-18.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-18.jpg" alt="Gallery Thumb 1"></a>
-                                <a href="images/Laklak/Meryem/F94T0300.JPG" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/F94T0300.JPG" alt="Gallery Thumb 2"></a>
-                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-20.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-20.jpg" alt="Gallery Thumb 3"></a>
-                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-19.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-19.jpg" alt="Gallery Thumb 4"></a>
-                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-13.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-13.jpg" alt="Gallery Thumb 5"></a>
-                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-12.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-12.jpg" alt="Gallery Thumb 6"></a>
-                                <a href="images/Laklak/Meryem/Meryem%20Aboulouafa.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem%20Aboulouafa.jpg" alt="Gallery Thumb 7"></a>
-                                <a href="images/Laklak/Meryem/JohnFPeters_BDMOROC_5828.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/JohnFPeters_BDMOROC_5828.jpg" alt="Gallery Thumb 9"></a>
-                            </div>
+                                            <div class="masonry-thumbs col-4" data-big="5" data-lightbox="gallery">
+                                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-18.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-18.jpg" alt="Gallery Thumb 1"></a>
+                                                <a href="images/Laklak/Meryem/F94T0300.JPG" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/F94T0300.JPG" alt="Gallery Thumb 2"></a>
+                                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-20.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-20.jpg" alt="Gallery Thumb 3"></a>
+                                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-19.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-19.jpg" alt="Gallery Thumb 4"></a>
+                                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-13.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-13.jpg" alt="Gallery Thumb 5"></a>
+                                                <a href="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-12.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem_Aboulouafa©BaptisteDVA-12.jpg" alt="Gallery Thumb 6"></a>
+                                                <a href="images/Laklak/Meryem/Meryem%20Aboulouafa.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/Meryem%20Aboulouafa.jpg" alt="Gallery Thumb 7"></a>
+                                                <a href="images/Laklak/Meryem/JohnFPeters_BDMOROC_5828.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/Laklak/Meryem/JohnFPeters_BDMOROC_5828.jpg" alt="Gallery Thumb 9"></a>
+                                            </div>
 
-                              <div class="divider"><i class="icon-circle"></i></div>
-                          </div>
+                                              <div class="divider"><i class="icon-circle"></i></div>
+                                          </div>
+                                </div>
                     </div>
-
-
-             </div>
     </div>
 
 
