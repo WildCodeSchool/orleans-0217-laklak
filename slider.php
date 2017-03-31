1693161983f5eb402601f243f9a7d5259bace264
Nicolas <lf.nicolas45@gmail.com>
2017-03-31 11:07:40 +0200

    modif

--- slider.php
+++ slider.php
@@ -23,14 +23,7 @@
                         <div class="slider-caption slider-caption-center swiper_wrapper">
                             <h2 data-caption-animate="fadeInUp">LAKLAK PRODUCTION</h2>
                             <p data-caption-animate="fadeInUp" data-caption-delay="200">Flexibility to the core &amp; different in every aspect. Meet the new Canvas. Creative &amp; Efficiently Powerful.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide" style="background-image: url('images/Laklak/Slider/2r.jpg');">
-                    <div class="container clearfix">
-                        <div class="slider-caption slider-caption-center swiper_wrapper">
-                            <h2 data-caption-animate="fadeInUp">LAKLAK PRODUCTION</h2>
-                            <p data-caption-animate="fadeInUp" data-caption-delay="200">Flexibility to the core &amp; different in every aspect. Meet the new Canvas. Creative &amp; Efficiently Powerful.</p>
+                            <a data-caption-animate="fadeInUp" data-caption-delay="300" href="#" class="button button-border button-rounded">View our Works</a>
                         </div>
                     </div>
                 </div>
