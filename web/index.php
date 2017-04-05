1a4ebe7ed2d18a193c9f64a1d0b79fda4e615606
Nicolas <lf.nicolas45@gmail.com>
2017-04-05 13:17:04 +0200

    Mise en place de la partie Admin

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 use laklak\controller\DefaultController;
+use laklak\controller\AdminController;
 
 
 
@@ -10,18 +11,44 @@ if (isset($_GET['page'])) {
     $page = $_GET['page'];
 }
 
-$default = new DefaultController();
 
-switch ($page) {
-    case 'presentation':
-        $view = $default->presentation();
-        break;
-    case 'agenda':
-        $view = $default->agenda();
-        break;
-    default :
-        $view = $default->index();
+//Si l'admin est connecté (simulé par le Get['id'] )
+//On redirige l'admin vers les pages admin
+if(isset($_GET['id'])){
+
+    $default = new AdminController(false);
+
+    switch ($page) {
+        case 'index':
+            $view = $default->index();
+            break;
+        case 'agenda':
+            $view = $default->index();
+            break;
+        default :
+            $view = $default->index();
+
+    }
+}
+//Sinon, on redirige l'utilisateur vers les pages du front
+else {
+
+    $default = new DefaultController(true);
+
+    switch ($page) {
+        case 'presentation':
+            $view = $default->presentation();
+            break;
+        case 'agenda':
+            $view = $default->agenda();
+            break;
+        default :
+            $view = $default->index();
+
+    }
 
 }
+
+
 
 echo $view;
