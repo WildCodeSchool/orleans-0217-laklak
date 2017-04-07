3c82c706934614ee618230c9c0adfb8100918261
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-04-07 11:06:02 +0200

    event eventcontroller eventmanager

---
+++ src/Model/EventManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder7
+ * Date: 07/04/17
+ * Time: 09:51
+ */
+
+namespace laklak\Model;
+
+
+class EventManager
+{
+
+}
---
+++ src/controller/EventController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder7
+ * Date: 07/04/17
+ * Time: 10:22
+ */
