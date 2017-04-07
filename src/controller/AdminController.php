4966e4d06c947a59eb68e9170e9ae73352ccd2f1
Nicolas <lf.nicolas45@gmail.com>
2017-04-07 17:03:09 +0200

    changement des twig

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -14,13 +14,13 @@ class AdminController extends Controller
 
     public function index()
     {
-        return $this->twig->render('index.html.twig');
+        return $this->getTwig()->render('index.html.twig');
     }
 
 
     public function adminAPropos()
     {
-        return $this->twig->render('adminapropos.html.twig');
+        return $this->getTwig()->render('adminapropos.html.twig');
     }
 
     public function addArtist()
@@ -34,7 +34,7 @@ class AdminController extends Controller
             // redirect vers la page qui liste les artist $this->listArtist()
 
         // sinon le form est pas submit, j'affiche le form
-        return $this->twig->render('ajoutartistes.html.twig');
+        return $this->getTwig()->render('ajoutartistes.html.twig');
     }
 
     public function updateArtist($id)
@@ -46,27 +46,27 @@ class AdminController extends Controller
             //  redirect
 
         // sinon j'affiche le form
-       // return $this->twig->render('ajoutartistes.html.twig', array('artist'=>$artist));
+       // return $this->getTwig()->render('ajoutartistes.html.twig', array('artist'=>$artist));
     }
 
     public function listeartistes()
     {
-        return $this->twig->render('listeartistes.html.twig');
+        return $this->getTwig()->render('listeartistes.html.twig');
     }
 
     public function listEvent()
     {
-        return $this->twig->render('listEvent.html.twig');
+        return $this->getTwig()->render('listEvent.html.twig');
     }
     public function deconnexion()
     {
         session_destroy();
-        return $this->twig->render('deconnexion.html.twig');
+        return $this->getTwig()->render('deconnexion.html.twig');
     }
 
     public function ajoutartistes()
     {
-        return $this->twig->render('ajoutartistes.html.twig');
+        return $this->getTwig()->render('ajoutartistes.html.twig');
     }
 
 
