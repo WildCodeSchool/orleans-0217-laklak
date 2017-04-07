c1d6d10831888ea6494e3d79d3422bbf774b152d
Frankois21 <francois.ponceau@gmail.com>
2017-04-07 18:03:21 +0200

    rien de neuf

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -69,6 +69,9 @@ class AdminController extends Controller
         return $this->twig->render('ajoutartistes.html.twig');
     }
 
-
+    public function listeevenements()
+    {
+        return $this->twig->render('listeevenements.html.twig');
+    }
 
 }
